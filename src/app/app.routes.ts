bdd1570493b09c433054dd0305e0bd851ac25a31
Vibeke <vise248@gmail.com>
2026-09-17 15:52:08 +0200

    feat/route to header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ColorTest } from './components/color-test/color-test';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'color-test', component: ColorTest }];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
