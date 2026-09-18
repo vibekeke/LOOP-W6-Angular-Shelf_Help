2f360e44feddb41af367170a8f9f7f4d7cac045b
Vibeke <vise248@gmail.com>
2026-09-18 13:24:23 +0200

    refactor/make dev folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
-import { ColorTest } from './components/color-test/color-test';
+import { ColorTest } from './components/dev/color-test/color-test';
+import { Landing } from './pages/landing/landing';
+import { Login } from './pages/login/login';
+import { BookCatalogue } from './pages/book-catalogue/book-catalogue';
+import { ReadingList } from './pages/reading-list/reading-list';
+import { BookDetails } from './pages/book-details/book-details';
 
-export const routes: Routes = [{ path: 'color-test', component: ColorTest }];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'login', component: Login },
+  { path: 'book-catalogue', component: BookCatalogue },
+  { path: 'reading-list', component: ReadingList },
+  { path: 'book-details/:id', component: BookDetails },
+  { path: 'color-test', component: ColorTest },
+];
