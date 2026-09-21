e200c48549a64563fa8acd3cfc51ae4b360d36bb
Vibeke <vise248@gmail.com>
2026-09-21 13:21:41 +0200

    wip: add login functionality

---
+++ src/app/services/auth.ts
@@ -0,0 +1,19 @@
+import { computed, Injectable, signal } from "@angular/core";
+
+export interface User {
+    username: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class Auth {
+    private readonly _user = signal<User | null>(null);
+    
+    readonly user = this._user.asReadonly();
+    readonly isLoggedIn = computed(() => this._user() !== null);
+
+    login(username: string) {
+        this._user.set({ username });
+    }
+
+    logout() {this._user.set(null);}
+}
