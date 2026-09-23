c5241eeea26ea190cfde2ee2d8a8e6270532994b
Vibeke <vise248@gmail.com>
2026-09-23 16:02:36 +0200

    wip/add book details page

--- src/app/pages/book-details/book-details.ts
+++ src/app/pages/book-details/book-details.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { Book, BookService } from '../../services/book-service';
+import { finalize } from 'rxjs';
 
 @Component({
   imports: [],
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './book-details.css',
   templateUrl: './book-details.html',
 })
-export class BookDetails {}
+export class BookDetails {
+  readonly id = input.required<string>();
+  private readonly bookService = inject(BookService);
+  
+  protected readonly errorMsg = signal<string | null> (null);
+  protected readonly isLoading = signal(false);
+  
+  protected book = signal<Book | null>(null);
+
+  ngOnInit(): void {
+    this.isLoading.set(true)
+
+    this.bookService.getBook(this.id()).pipe(
+      finalize(() => this.isLoading.set(false))
+    ).subscribe({
+      next: (book) => this.book.set(book),
+      error: (err) => {
+        console.error('BookDetails: Fetch failed,', err)
+        this.errorMsg.set('Could not load your book :(')
+      }
+    });
+  }
+}
