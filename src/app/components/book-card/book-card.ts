3a101a980c6595fe9afa52c1e5c1e22a4c3288ea
Vibeke <vise248@gmail.com>
2026-09-24 11:26:05 +0200

    refactor/extract add to collection button

--- src/app/components/book-card/book-card.ts
+++ src/app/components/book-card/book-card.ts
@@ -1,43 +1,20 @@
-import { Component, computed, inject, input, signal } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Book } from '../../services/book-service';
 import { UserService } from '../../services/user-service';
-import { finalize } from 'rxjs';
+import { CollectionButton } from '../collection-button/collection-button';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, CollectionButton],
   selector: 'app-book-card',
   styleUrl: './book-card.css',
   templateUrl: './book-card.html',
 })
 export class BookCard {
 
-  readonly userService = inject(UserService);
+  private readonly userService = inject(UserService);
   readonly book = input.required<Book>();
 
-  readonly inReadingList = computed(() => 
-    this.userService.user()?.collection.includes(this.book().id) ?? false);
-
-  protected readonly isLoadingList = signal(false); //Why does this need to be a signal?
-
-
-  addToReadingList() {
-    this.isLoadingList.set(true);
-
-    this.userService.addToCollection(this.book().id).pipe(
-      finalize(() => this.isLoadingList.set(false))
-    ).subscribe({
-      error: (err) => console.error('List operation failed', err),
-    })
-  }
-
-  removeFromReadingList() {
-    this.isLoadingList.set(true);
-
-    this.userService.deleteFromCollection(this.book().id).pipe(
-      finalize(() => this.isLoadingList.set(false))
-    ).subscribe({
-      error: (err) => console.error('List operation failed', err),
-    })
-  }
+  protected readonly inReadingList = computed(() =>
+    this.userService.isInCollection(this.book().id));
 }
---
+++ src/app/components/collection-button/collection-button.ts
@@ -0,0 +1,40 @@
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { finalize } from 'rxjs';
+import { Book } from '../../services/book-service';
+import { UserService } from '../../services/user-service';
+
+@Component({
+  imports: [],
+  selector: 'app-collection-button',
+  styleUrl: './collection-button.css',
+  templateUrl: './collection-button.html',
+})
+export class CollectionButton {
+  readonly book = input.required<Book>();
+  private readonly userService = inject(UserService);
+
+  protected readonly inReadingList = computed(() =>
+    this.userService.isInCollection(this.book().id));
+  protected readonly isLoadingList = signal(false);
+
+
+  protected addToReadingList() {
+    this.isLoadingList.set(true);
+
+    this.userService.addToCollection(this.book().id).pipe(
+      finalize(() => this.isLoadingList.set(false))
+    ).subscribe({
+      error: (err) => console.error('List operation failed', err),
+    })
+  }
+
+  protected removeFromReadingList() {
+    this.isLoadingList.set(true);
+
+    this.userService.deleteFromCollection(this.book().id).pipe(
+      finalize(() => this.isLoadingList.set(false))
+    ).subscribe({
+      error: (err) => console.error('List operation failed', err),
+    })
+  }
+}
