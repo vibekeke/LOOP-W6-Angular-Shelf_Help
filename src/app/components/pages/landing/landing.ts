f8967e8f9d3640f9340b4447cca27861f3278a94
Vibeke <vise248@gmail.com>
2026-09-24 13:30:17 +0200

    feat/add example book cards to front page

--- src/app/components/pages/landing/landing.ts
+++ src/app/components/pages/landing/landing.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { UserService } from '../../../services/user-service';
+import { BookService } from '../../../services/book-service';
+import { finalize } from 'rxjs';
+import { BookCard } from '../../book-card/book-card';
 
 @Component({
-  imports: [],
+  imports: [BookCard],
   selector: 'app-landing',
   styleUrl: './landing.css',
   templateUrl: './landing.html',
 })
-export class Landing {}
+export class Landing {
+  private readonly userService = inject(UserService);
+  private readonly bookService = inject(BookService);
+
+  protected readonly isLoggedIn = computed(() => 
+    this.userService.isLoggedIn()
+  );
+
+  protected readonly errorMsg = signal<string | null> (null);
+  protected readonly isLoading = signal(false);
+
+  readonly books = this.bookService.books
+
+  readonly randomBooks = computed(() => {
+    const copy = this.books();
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+    return copy.slice(0, 3);
+  });
+
+  ngOnInit(): void {
+    this.isLoading.set(true)
+    this.bookService.fetchBooks().pipe(
+      finalize(() => this.isLoading.set(false))
+    ).subscribe(
+      {
+        error: (err) => {
+        console.error('BookCatalogue: Fetch failed', err)
+        this.errorMsg.set("Could not load books. Try again later");
+      }}
+    ); 
+  }
+}
